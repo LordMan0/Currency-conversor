251cbd887cc9084c2b9f56e0c466bb519e2fd891
Gabriel D. M <gmanoel057@gmail.com>
2021-03-18 10:32:35 -0300

    App version 1.0

--- currency-conversor-js/app.js
+++ currency-conversor-js/app.js
@@ -1,7 +1,8 @@
 const currencyOneSelector = document.querySelector('[data-js="currency-one"]')
 const currencyTwoSelector = document.querySelector('[data-js="currency-two"]')
-const currencyTimes = document.querySelector('[data-js="currency-times"]')
+const numberTimesCurrency = document.querySelector('[data-js="currency-one-times"]')
 const convertedValueEl = document.querySelector('[data-js="converted-value"]')
+const conversionPrecisionEl = document.querySelector('[data-js="conversion-precision"]')
 
 const apiKey = '8a4f7eac12f86a8d5f9b0db8'
 const baseUrl = currency => 
@@ -25,18 +26,51 @@ const addOption = (selector, currency, selectedCurrency) => {
 }
 
 
-const init = async () => {
-    const exchangeRateData = await fetchExchangeRate('USD')
-
+const showExchageRate = async (currencyOne, currencyTwo) => {
+    const exchangeRateData = await fetchExchangeRate(currencyOne)
+    const currencyList = exchangeRateData.conversion_rates
+    const exchangeRate = currencyList[currencyTwo]
+    
     const getOptionsFromData = (selector, selectedCurrency) => 
-        Object.keys(exchangeRateData.conversion_rates)
-        .map(currency => addOption(selector, currency, selectedCurrency))
+        Object.keys(currencyList)
+            .map(currency => addOption(selector, currency, selectedCurrency))
+    
+    getOptionsFromData(currencyOneSelector, currencyOne)
+    getOptionsFromData(currencyTwoSelector, currencyTwo)
+    
 
-    getOptionsFromData(currencyOneSelector, 'USD')
-    getOptionsFromData(currencyTwoSelector, 'BRL')
+    convertedValueEl.textContent = exchangeRate.toFixed(2)
 
-    convertedValueEl.textContent = exchangeRateData.conversion_rates.BRL.toFixed(2)
-    console.log(currencyOneSelector.value)
+    numberTimesCurrency.addEventListener('input', e => {
+        const inputValue = e.target.value
+
+        if (inputValue > 0) {
+            convertedValueEl.textContent = (inputValue * exchangeRate)
+                .toFixed(2)
+        }
+    })
+
+    conversionPrecisionEl.textContent = 
+        `1 ${currencyOne} = ${exchangeRate} ${currencyTwo}`
 }
 
-init()
+const startDefaultExchange = () => {
+    const defaultCurrencyOne = 'USD'
+    const defaultCurrencyTwo = 'BRL'
+
+    showExchageRate(defaultCurrencyOne, defaultCurrencyTwo)
+}
+
+startDefaultExchange()
+
+currencyOneSelector.addEventListener('input', e => {
+    const currencyTwo = currencyTwoSelector
+        .options[currencyTwoSelector.selectedIndex].value
+    showExchageRate(e.target.value, currencyTwo)
+})
+
+currencyTwoSelector.addEventListener('input', e => {
+    const currencyOne = currencyOneSelector
+        .options[currencyOneSelector.selectedIndex].value
+    showExchageRate(currencyOne, e.target.value)
+})
